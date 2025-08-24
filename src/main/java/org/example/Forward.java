1488211d433bcac057515cee66a42fdad32ca0f4
Nour eldin Osama Fathy <elananynour@gmail.com>
2025-08-24 22:09:30 +0300

    Store results

--- src/main/java/org/example/Forward.java
+++ src/main/java/org/example/Forward.java
@@ -3,4 +3,10 @@ package org.example;
 public class Forward {
     // input -> X_batch, W1, b1, W2, b2, W3, b3
     // output -> net1, out1, net2, out2, net3, out3
+    
+    public double[][] net1, out1;
+    public double[][] net2, out2;  
+    public double[][] net3, out3;
+    
+
 }
